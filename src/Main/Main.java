b99d99eaecf2787b0f459025a80513e221785a61
bmor505 <bmor505@wgu.edu>
2025-11-21 17:09:43 -0800

    Basic structure so far

---
+++ src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Loot2d");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
